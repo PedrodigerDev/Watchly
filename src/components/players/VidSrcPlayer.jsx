97b88b22a4148261ca090f586fe8a464ca364e6c
PedrodigerDev <139206036+PedrodigerDev@users.noreply.github.com>
2025-08-23 05:01:10 -0400

    Update VidSrcPlayer.jsx

--- src/components/players/VidSrcPlayer.jsx
+++ src/components/players/VidSrcPlayer.jsx
@@ -1,18 +1,17 @@
-const VidSrcPlayer = ({ imdbId, season, episode }) => {
-  // Movie vs TV paths (adjust if your app is only movies)
-  const path = season && episode
-    ? `https://vidsrc.me/embed/tv/${imdbId}/${season}-${episode}`
-    : `https://vidsrc.me/embed/movie/${imdbId}`;
+// src/components/players/VidSrcPlayer.jsx
+import { getVidSrcEmbed } from "../../utils/vidsrc";
+
+const VidSrcPlayer = ({ type, imdb, tmdb, season, episode, subUrl, dsLang, autoplay, autonext }) => {
+  const src = getVidSrcEmbed({ type, imdb, tmdb, season, episode, subUrl, dsLang, autoplay, autonext });
 
   return (
     <div style={{ position: "relative", paddingTop: "56.25%" }}>
       <iframe
-        src={path}
+        src={src}
         title="VidSrc Player"
         allow="autoplay; fullscreen; encrypted-media; picture-in-picture"
         allowFullScreen
         referrerPolicy="no-referrer"
-        // sandbox is optional; relax if controls break
         sandbox="allow-scripts allow-same-origin allow-forms allow-presentation allow-popups"
         style={{ position: "absolute", inset: 0, width: "100%", height: "100%", border: 0 }}
       />
